f90b6685454f3b31676e0dd7ac7affc59856761d
Tarun <Tarun@DESKTOP-LMGB0KT>
2017-01-23 03:32:51 +0530

    Initial Checkin

---
+++ knapshack/src/main/java/com/problem/dbs/KnapshackApplication.java
@@ -0,0 +1,12 @@
+package com.problem.dbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnapshackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnapshackApplication.class, args);
+	}
+}
---
+++ knapshack/src/test/java/com/problem/dbs/KnapshackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.problem.dbs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnapshackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
